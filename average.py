1f7b0a3c47a39f2c06ae54aa4991012224750034
edbe777 <61211582+edbe777@users.noreply.github.com>
2020-09-28 12:08:25 -0700

    Initial commit

---
+++ average.py
@@ -0,0 +1,12 @@
+#Eddie Beloiu
+#CS 161
+# #project 2a
+
+print("Please enter 5 numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+average = (num_1 + num_2 + num_3 + num_4 + num_5) / 5
+print("The average of those numbers is:", average)
